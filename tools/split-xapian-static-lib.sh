e88cfd55f14bcaa2044f91f901d3be748c330f3f
itiu <ValeriyBushenev@gmail.com>
2016-06-27 11:39:21 +0300

    * change to 1.4

--- tools/split-xapian-static-lib.sh
+++ tools/split-xapian-static-lib.sh
@@ -1,11 +1,15 @@
 ar x libxapian.a
 l1=`ar t libxapian.a |  grep brass`
 l2=`ar t libxapian.a |  grep flint`
+l3=`ar t libxapian.a |  grep chert`
+l4=`ar t libxapian.a |  grep glass`
 
-ar rvs libxapian-backend.a $l1 $l2
+ar rvs libxapian-backend.a $l1 $l2 $l3 $l4
 
 rm $l1
 rm $l2
+rm $l3
+rm $l4
 
 ar rvs libxapian-main.a *.o
 
